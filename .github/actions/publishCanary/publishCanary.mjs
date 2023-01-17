e631bc3738faef9c630d7068b2c5e9643b8b9c6f
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-01-17 14:09:02 -0800

    wip

--- .github/actions/publishCanary/publishCanary.mjs
+++ .github/actions/publishCanary/publishCanary.mjs
@@ -1,10 +1,14 @@
-import { exec, getExecOutput } from "@actions/exec";
+import { exec, getExecOutput } from '@actions/exec'
+
+/**
+ * - can use shell here?
+ */
 
 async function run() {
-  const args = ["premajor"];
+  const args = ['premajor']
 
-  // @TODO—can use shell? not the end of the world
-
+  // ------------------------
+  // Get the latest version
   // Returns a string like 'v3.8.0'.
   /**
    * @type {`v${number}.${number}.${number}`}
@@ -12,50 +16,52 @@ async function run() {
   const { stdout } = await getExecOutput(
     `git tag --sort="-version:refname" --list "v?.?.?" | head -n 1`,
     null,
-    { shell: true },
+    { shell: true }
   )
 
-  const latestRelease = stdout.trim();
+  const latestRelease = stdout.trim()
 
   // Get the major version from a string like 'v3.8.0'.
   const currentMajor = +latestRelease.match(/^v(?<currentMajor>\d)\./).groups
-    .currentMajor;
-  const nextMajor = `${currentMajor + 1}.0.0`;
+    .currentMajor
+  const nextMajor = `${currentMajor + 1}.0.0`
 
-  console.log({ currentMajor, nextMajor });
+  console.log({ currentMajor, nextMajor })
 
+  // ------------------------
   // Get the latest RC from NPM.
   /**
    * @type {{ name: string, version: `${number}.${number}.${number}` }}
    */
-
-  await getExecOutput('yarn npm info @redwoodjs/core@rc --fields version --json')
-  const { version: latestRC } = JSON.parse(
-  );
+  await getExecOutput(
+    'yarn npm info @redwoodjs/core@rc --fields version --json'
+  )
+  const { version: latestRC } = JSON.parse()
 
   if (latestRC.startsWith(nextMajor)) {
     console.log(
-      "The latest rc is the same as the canary; adding an extra minor to the canary",
-    );
+      'The latest rc is the same as the canary; adding an extra minor to the canary'
+    )
 
-    args.push("--rw-custom-bump");
+    args.push('--rw-custom-bump')
   }
 
-  args = [
-    ...args,
-    "--include-merged-tags",
-    "--canary",
+  args.concat([
+    '--include-merged-tags',
+    '--canary',
     `--preid canary`,
     `--dist-tag canary`,
-    "--force-publish",
-    "--loglevel verbose",
-    "--no-git-reset",
-    "--yes",
-  ];
+    '--force-publish',
+    '--loglevel verbose',
+    '--no-git-reset',
+    '--yes',
+  ])
 
-  console.log({ args });
+  console.log({ args })
 
-  await exec(`yarn lerna publish ${args.join(" ")}`);
+  // ------------------------
+  // Publish
+  await exec(`yarn lerna publish ${args.join(' ')}`)
 }
 
-run();
+run()
