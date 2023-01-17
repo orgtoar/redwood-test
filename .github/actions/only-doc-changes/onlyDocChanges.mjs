21955833bb15c5cd333f328d4966dddab06915a1
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-01-17 14:09:02 -0800

    wip

--- .github/actions/only-doc-changes/onlyDocChanges.mjs
+++ .github/actions/only-doc-changes/onlyDocChanges.mjs
@@ -1,39 +1,41 @@
-import core from "@actions/core";
-import { exec, getExecOutput } from "@actions/exec";
+import core from '@actions/core'
+import { exec, getExecOutput } from '@actions/exec'
 
 // Docs that aren't in the docs directory
-const ignoreFiles = [
-  "CHANGELOG.md",
-  "CODE_OF_CONDUCT.md",
-  "CONTRIBUTING.md",
-  "CONTRIBUTORS.md",
-  "LICENSE",
-  "README.md",
-  "SECURITY.md",
-];
+const IGNORE_FILES = /** @type {const} */ ([
+  'CHANGELOG.md',
+  'CODE_OF_CONDUCT.md',
+  'CONTRIBUTING.md',
+  'CONTRIBUTORS.md',
+  'LICENSE',
+  'README.md',
+  'SECURITY.md',
+])
 
 async function run() {
-  const branch = process.env.GITHUB_BASE_REF;
+  const branch = process.env.GITHUB_BASE_REF
 
-  await exec(`git fetch origin ${branch}`);
-  console.log();
-
-  const { stdout } = await getExecOutput(
-    `git diff origin/${branch} --name-only`,
-  );
+  await exec(`git fetch origin ${branch}`)
   console.log()
 
   // A PR is considered to only have doc changes if it...
   // 1. only changes files in the docs directory
-  // 2. only changes files in `ignoreFiles`
-  const changedFiles = stdout.trim().split("\n")
+  // 2. only changes files in `IGNORE_FILES`
+  const { stdout } = await getExecOutput(
+    `git diff origin/${branch} --name-only`
+  )
+  console.log()
+
+  const changedFiles = stdout
+    .trim()
+    .split('\n')
     .filter(Boolean)
-    .filter((changedFile) => changedFile.startsWith("docs"))
-    .filter((changedFile) => ignoreFiles.includes(changedFile));
+    .filter((changedFile) => changedFile.startsWith('docs'))
+    .filter((changedFile) => IGNORE_FILES.includes(changedFile))
 
   console.log({ changedFiles })
 
-  core.setOutput("only-doc-changes", !!changedFiles.length);
+  core.setOutput('only-doc-changes', !!changedFiles.length)
 }
 
-run();
+run()
