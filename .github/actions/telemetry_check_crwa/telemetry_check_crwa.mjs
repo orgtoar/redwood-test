4b4da54601192fd2debf6260c383b0da2ea7f51a
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-01-28 17:26:56 +0000

    wip

--- .github/actions/telemetry_check_crwa/telemetry_check_crwa.mjs
+++ .github/actions/telemetry_check_crwa/telemetry_check_crwa.mjs
@@ -10,25 +10,30 @@ console.log(process.env.REDWOOD_REDIRECT_TELEMETRY)
 
 // Build the create-redwood-app package
 // TODO: Only build CRWA here?
-await exec(`yarn build`)
+// await exec(`yarn build`)
 
 // Setup fake telemetry server
-const requestListener = function (req, res) {
-  console.log("\n------", req.body, "\n------")
+const server = http.createServer((req, res) => {
+  console.log("------")
+  console.log(req.body)
+  console.log("------")
 
   res.writeHead(200)
   res.end()
 
   // TODO: test telemetry packet structure
-  // TODO: exit(0)
-}
-const server = http.createServer(requestListener);
+  process.exit(0)
+});
 server.listen(7777, "localhost", () => {
   console.log(`Server is running on http://localhost:7777`);
 });
 
 // Run create-redwood-app
-await exec(`yarn node ./packages/create-redwood-app/dist/create-redwood-app.js ../crwa-telemetry --typescript true --git true`)
+try {
+  await exec(`yarn node ./packages/create-redwood-app/dist/create-redwood-app.js ../crwa-telemetry --typescript true --git true`)
+} catch (error) {
+  console.error(error)
+}
 
 await new Promise(r => setTimeout(r, 5_000));
 process.exit(1)
