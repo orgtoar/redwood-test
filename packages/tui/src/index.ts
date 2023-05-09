ecb1e84788fe170f0efce8c337a17764b21ef84f
Josh-Walker-GM <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-05-09 01:59:19 +0100

    wip: test less spam for non-tty

--- packages/tui/src/index.ts
+++ packages/tui/src/index.ts
@@ -216,6 +216,12 @@ export class RedwoodTUI {
       throw new Error('TUI has no reactive content')
     }
 
+    // Only draw once if the TUI is not a TTY
+    if (!this.outStream.isTTY) {
+      this.drawReactive(true)
+      return
+    }
+
     if (!this.manager.isHooked) {
       // Take control of the terminal
       this.manager.hook()
