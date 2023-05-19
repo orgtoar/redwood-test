dc6c99351b08f818f7315f94239353f4228a3c97
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-18 21:36:20 -0700

    continue

--- .github/actions/setup_test_project/setup_test_project.mjs
+++ .github/actions/setup_test_project/setup_test_project.mjs
@@ -8,8 +8,6 @@ import * as core from '@actions/core'
 const test_project_path = path.join(
   os.tmpdir(),
   'test-project',
-  // ":" is problematic with paths
-  new Date().toISOString().split(':').join('-')
 )
 
 console.log({
@@ -20,13 +18,6 @@ core.setOutput('test_project_path', test_project_path)
 
 try {
   await exec(`yarn build:test-project --link ${test_project_path}`)
-
-  const dir = fs.readdirSync(path.join(test_project_path, 'web', 'src'))
-
-  console.log('------------------------')
-  console.log('readdirSync')
-  console.log(JSON.stringify(dir))
-
 } catch(e) {
   console.error(e)
   process.exitCode = 1
