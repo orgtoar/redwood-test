89711c5c7f326f0699664941a68d0fc098fbe0cc
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-18 21:44:37 -0700

    continue

--- .github/actions/setup_test_project/setup_test_project.mjs
+++ .github/actions/setup_test_project/setup_test_project.mjs
@@ -5,14 +5,11 @@ import fs from 'fs'
 import { exec } from '@actions/exec'
 import * as core from '@actions/core'
 
-const test_project_path = path.join(
-  os.tmpdir(),
-  'test-project',
-)
+const test_project_path = path.join(os.tmpdir(), 'test-project')
 
-console.log({
-  test_project_path
-})
+console.log('-'.repeat(process.stdout.columns))
+console.log({ test_project_path })
+console.log('-'.repeat(process.stdout.columns))
 
 core.setOutput('test_project_path', test_project_path)
 
