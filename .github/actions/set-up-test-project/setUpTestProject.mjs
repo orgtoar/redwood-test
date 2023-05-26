fa6aa3a5fe6dcdb10d262359d84b665925942bcc
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-26 02:12:38 -0700

    add cwd

--- .github/actions/set-up-test-project/setUpTestProject.mjs
+++ .github/actions/set-up-test-project/setUpTestProject.mjs
@@ -19,10 +19,6 @@ const REDWOOD_PROJECT_PATH = path.join(
   new Date().toISOString().split(':').join('-')
 )
 
-console.log({
-  REDWOOD_PROJECT_PATH
-})
-
 core.setOutput('TEST_PROJECT_PATH', REDWOOD_PROJECT_PATH)
 
 console.log(`Creating project at ${REDWOOD_PROJECT_PATH}`)
@@ -30,22 +26,48 @@ await fs.copy(TEST_PROJECT_FIXTURE_PATH, REDWOOD_PROJECT_PATH)
 console.log()
 
 console.log(`Adding framework dependencies to ${REDWOOD_PROJECT_PATH}`)
-await exec('yarn project:deps', null, { env: { RWJS_CWD: REDWOOD_PROJECT_PATH } })
+await exec(
+  'yarn project:deps',
+  null,
+  {
+    cwd: REDWOOD_FRAMEWORK_PATH,
+    env: {
+      RWJS_CWD: REDWOOD_PROJECT_PATH
+    }
+  }
+)
 console.log()
 
 console.log(`Installing node_modules in ${REDWOOD_PROJECT_PATH}`)
 await exec('yarn install', null, { cwd: REDWOOD_PROJECT_PATH })
 
 console.log('Copying framework packages to project')
-await exec('yarn project:copy', null, { env: { RWJS_CWD: REDWOOD_PROJECT_PATH } })
+await exec(
+  'yarn project:copy',
+  null,
+  {
+    cwd: REDWOOD_FRAMEWORK_PATH,
+    env: {
+      RWJS_CWD: REDWOOD_PROJECT_PATH
+    }
+  }
+)
 console.log()
 
 console.log('Generating dbAuth secret')
-const { stdout } = await getExecOutput('yarn rw g secret --raw', null, { cwd: REDWOOD_PROJECT_PATH })
+const { stdout } = await getExecOutput(
+  'yarn rw g secret --raw',
+  null,
+  { cwd: REDWOOD_PROJECT_PATH }
+)
 fs.appendFileSync(
   path.join(REDWOOD_PROJECT_PATH, '.env'),
   `SESSION_SECRET='${stdout}'`
 )
 
 console.log('Running prisma migrate reset')
-await exec('yarn rw prisma migrate reset --force', null, { cwd: REDWOOD_PROJECT_PATH })
+await exec(
+  'yarn rw prisma migrate reset --force',
+  null,
+  { cwd: REDWOOD_PROJECT_PATH }
+)
