e7c2beee626a9e7cb9f3e3f20435e8bd38b85043
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-27 14:21:53 -0700

    make yarn run checks nicer

--- tasks/run-yarn-checks/runYarnChecks.mjs
+++ tasks/run-yarn-checks/runYarnChecks.mjs
@@ -7,24 +7,26 @@ import { exec } from '@actions/exec'
 const checks = [
   {
     command: 'yarn constraints',
-    fix: 'You can fix this by running "yarn constraints --fix"',
+    fix: 'You can fix this by running `yarn constraints --fix`',
   },
   {
     command: 'yarn dedupe --check',
-    fix: 'You can fix this by running "yarn dedupe"',
+    fix: 'You can fix this by running `yarn dedupe`',
   },
   {
     command: 'yarn workspaces foreach --parallel run sort-package-json --check',
-    fix: 'You can fix this by running "yarn workspaces foreach --parallel dlx sort-package-json"',
+    fix: 'You can fix this by running `yarn workspaces foreach --parallel dlx sort-package-json`',
   },
 ]
 
 for (const { command, fix } of checks) {
   try {
     await exec(command)
-    console.log(`"${command}" passed`)
+    console.log()
   } catch (_e) {
+    console.log()
     core.setFailed(`"${command}" failed`)
     console.log(fix)
+    console.log()
   }
 }
