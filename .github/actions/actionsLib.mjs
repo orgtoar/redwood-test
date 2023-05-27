2790a565da6c675be231016b1801833977be3a45
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-27 00:03:55 -0700

    add lib

---
+++ .github/actions/actionsLib.mjs
@@ -0,0 +1,51 @@
+/* eslint-env node */
+// @ts-check
+
+import { fileURLToPath } from 'node:url'
+import { getExecOutput } from '@actions/exec'
+
+/**
+ * @typedef {import('@actions/exec').ExecOptions} ExecOptions
+ */
+
+const REDWOOD_FRAMEWORK_PATH = fileURLToPath(new URL('../../../', import.meta.url))
+
+/**
+ * @param {string} command
+ * @param {ExecOptions} options
+ */
+function execWithEnv(command, { env = {}, ...rest } = {}) {
+  return getExecOutput(
+    command,
+    undefined,
+    {
+      // @ts-expect-error TS doesn't like spreading process.env here but it's fine
+      env: {
+        ...process.env,
+        ...env
+      },
+      ...rest
+    }
+  )
+}
+
+/**
+ * @param {string} cwd
+ */
+function createExecWithEnvInCwd(cwd) {
+  /**
+   * @param {string} command
+   * @param {Omit<ExecOptions, 'cwd'>} options
+   */
+  return function (command, options = {}) {
+    return execWithEnv(command, { cwd, ...options })
+  }
+}
+
+const execInFramework = createExecWithEnvInCwd(REDWOOD_FRAMEWORK_PATH)
+
+export {
+  REDWOOD_FRAMEWORK_PATH,
+  execInFramework,
+  createExecWithEnvInCwd,
+}
--- .github/actions/set-up-test-project/setUpTestProject.mjs
+++ .github/actions/set-up-test-project/setUpTestProject.mjs
@@ -2,14 +2,16 @@
 // @ts-check
 
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
+import core from '@actions/core'
 import fs from 'fs-extra'
 
-import * as core from '@actions/core'
-import { exec, getExecOutput } from '@actions/exec'
+import {
+  REDWOOD_FRAMEWORK_PATH,
+  execInFramework,
+  createExecWithEnvInCwd
+} from '../actionsLib.mjs'
 
-const REDWOOD_FRAMEWORK_PATH = fileURLToPath(new URL('../../../', import.meta.url))
 const TEST_PROJECT_FIXTURE_PATH = path.join(
   REDWOOD_FRAMEWORK_PATH,
   '__fixtures__',
@@ -17,47 +19,54 @@ const TEST_PROJECT_FIXTURE_PATH = path.join(
 )
 const REDWOOD_PROJECT_PATH = core.getInput('test-project-path')
 
-console.log(`Creating project at ${REDWOOD_PROJECT_PATH}`)
+const execInProject = createExecWithEnvInCwd(REDWOOD_PROJECT_PATH)
 
-await fs.copy(TEST_PROJECT_FIXTURE_PATH, REDWOOD_PROJECT_PATH)
-console.log()
+async function main() {
+  console.log(`Creating project at ${REDWOOD_PROJECT_PATH}`)
+  await fs.copy(TEST_PROJECT_FIXTURE_PATH, REDWOOD_PROJECT_PATH)
+  console.log()
 
-console.log(`Adding framework dependencies to ${REDWOOD_PROJECT_PATH}`)
-await run('yarn project:deps')
-console.log()
-
-console.log(`Installing node_modules in ${REDWOOD_PROJECT_PATH}`)
-await run('yarn install', { cwd: REDWOOD_PROJECT_PATH })
-console.log()
-
-console.log('Copying framework packages to project')
-await run('yarn project:copy')
-console.log()
-
-console.log('Generating dbAuth secret')
-const { stdout } = await getExecOutput(
-  'yarn rw g secret --raw',
-  undefined,
-  { cwd: REDWOOD_PROJECT_PATH, silent: true }
-)
-fs.appendFileSync(
-  path.join(REDWOOD_PROJECT_PATH, '.env'),
-  `SESSION_SECRET='${stdout}'`
-)
-console.log()
-
-console.log('Running prisma migrate reset')
-await run(
-  'yarn rw prisma migrate reset --force',
-  { cwd: REDWOOD_PROJECT_PATH }
-)
-
-function run(command, { cwd = REDWOOD_FRAMEWORK_PATH, env = {} } = {}) {
-  return exec(command, undefined, {
-    cwd, env: {
-      ...process.env,
-      RWJS_CWD: REDWOOD_PROJECT_PATH,
-      ...env
+  console.log(`Adding framework dependencies to ${REDWOOD_PROJECT_PATH}`)
+  await execInFramework(
+    'yarn project:deps',
+    {
+      env: {
+        RWJS_CWD: REDWOOD_PROJECT_PATH
+      }
     }
-  })
+  )
+  console.log()
+
+  console.log(`Installing node_modules in ${REDWOOD_PROJECT_PATH}`)
+  await execInProject('yarn install')
+  console.log()
+
+  console.log('Copying framework packages to project')
+  await execInFramework(
+    'yarn project:copy',
+    {
+      env: {
+        RWJS_CWD: REDWOOD_PROJECT_PATH
+      }
+    }
+  )
+  console.log()
+
+  console.log('Generating dbAuth secret')
+  const { stdout } = await execInProject(
+    'yarn rw g secret --raw',
+    { silent: true }
+  )
+  fs.appendFileSync(
+    path.join(REDWOOD_PROJECT_PATH, '.env'),
+    `SESSION_SECRET='${stdout}'`
+  )
+  console.log()
+
+  console.log('Running prisma migrate reset')
+  await execInProject(
+    'yarn rw prisma migrate reset --force',
+  )
 }
+
+main()
