5753b6694061b70387df85f4ec51606686128e3f
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-26 22:53:42 -0700

    try telemetry changes

--- .github/actions/telemetry-check/telemetryCheck.mjs
+++ .github/actions/telemetry-check/telemetryCheck.mjs
@@ -1,4 +1,5 @@
 /* eslint-env node */
+// @ts-check
 
 import http from 'http'
 import path from 'path'
@@ -9,7 +10,7 @@ console.log(
   `Telemetry is being redirected to ${process.env.REDWOOD_REDIRECT_TELEMETRY}`
 )
 
-// Setup fake telemetry server
+// Set up fake telemetry server.
 const server = http.createServer((req, res) => {
   let data = ''
   req.on('data', (chunk) => {
@@ -23,14 +24,18 @@ const server = http.createServer((req, res) => {
   })
 })
 
-// Run the fake telemetry server at the redirected location
+// Run the fake telemetry server at the redirected location.
+
+// @ts-expect-error REDWOOD_REDIRECT_TELEMETRY is set by the action
 const host = process.env.REDWOOD_REDIRECT_TELEMETRY.split(':')[1].slice(2)
+// @ts-expect-error REDWOOD_REDIRECT_TELEMETRY is set by the action
 const port = parseInt(process.env.REDWOOD_REDIRECT_TELEMETRY.split(':')[2])
+
 server.listen(port, host, () => {
   console.log(`Telemetry listener is running on http://${host}:${port}`)
 })
 
-// Run a command and await output
+// Run a command and await output.
 try {
   const mode = process.argv[process.argv.indexOf('--mode') + 1]
   let exitCode = 0
@@ -39,26 +44,34 @@ try {
       exitCode = await exec(
         `yarn node ./packages/create-redwood-app/dist/create-redwood-app.js ../project-for-telemetry --typescript true --git false`
       )
+
       if (exitCode) {
         process.exit(1)
       }
+
       break
+
     case 'cli':
       exitCode = await exec(
-        `yarn install`, null, {
+        `yarn install`, undefined, {
           cwd: path.join(process.cwd(), '../project-for-telemetry')
         }
       )
+
       if (exitCode) {
         process.exit(1)
       }
+
       exitCode = await exec(
-        `yarn --cwd ../project-for-telemetry node ../redwood/packages/cli/dist/index.js info`
+        `yarn node ../redwood/packages/cli/dist/index.js info --cwd ../project-for-telemetry`
       )
+
       if (exitCode) {
         process.exit(1)
       }
+
       break
+
     default:
       console.error(`Unknown mode: ${mode}`)
       process.exit(1)
@@ -67,7 +80,7 @@ try {
   console.error(error)
 }
 
-// If we didn't hear the telemetry after 2 mins then let's fail
+// Fail if we didn't hear the telemetry after two minutes.
 await new Promise((r) => setTimeout(r, 120_000))
-console.error('No telemetry response within 120 seconds. Failing...')
-process.exit(1)
+console.error('No telemetry response within 120 seconds. Failing.')
+process.exitCode = 1
