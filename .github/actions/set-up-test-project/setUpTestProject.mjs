e24e442735431419f1a0d38357940de16e616842
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-27 11:37:08 -0700

    try improving

--- .github/actions/set-up-test-project/setUpTestProject.mjs
+++ .github/actions/set-up-test-project/setUpTestProject.mjs
@@ -1,19 +1,9 @@
 /* eslint-env node */
 // @ts-check
 
-import path from 'node:path'
-
 import cache from '@actions/cache'
 import core from '@actions/core'
 import { hashFiles } from '@actions/glob'
-import fs from 'fs-extra'
-
-import {
-  createExecWithEnvInCwd,
-  projectCopy,
-  projectDeps,
-  REDWOOD_FRAMEWORK_PATH,
-} from '../actionsLib.mjs'
 
 const TEST_PROJECT_PATH = core.getInput('test-project-path')
 
@@ -24,21 +14,36 @@ const key = [
   hashFiles('packages')
 ].join('-')
 
-const TEST_PROJECT_FIXTURE_PATH = path.join(
-  REDWOOD_FRAMEWORK_PATH,
-  '__fixtures__',
-  'test-project'
-)
-
-const execInProject = createExecWithEnvInCwd(TEST_PROJECT_PATH)
-
 async function main() {
-  const cacheKey = cache.restoreCache([TEST_PROJECT_PATH], key)
+  const cacheKey = await cache.restoreCache([TEST_PROJECT_PATH], key)
 
   if (cacheKey) {
     console.log(`Cache hit; restoring test project at ${TEST_PROJECT_PATH}`)
     return
   }
+
+  await setUpTestProject()
+}
+
+async function setUpTestProject() {
+  const path = await import('node:path')
+
+  const fs = await import('fs-extra')
+
+  const {
+    createExecWithEnvInCwd,
+    projectCopy,
+    projectDeps,
+    REDWOOD_FRAMEWORK_PATH,
+  } = await import('../actionsLib.mjs')
+
+  const TEST_PROJECT_FIXTURE_PATH = path.join(
+    REDWOOD_FRAMEWORK_PATH,
+    '__fixtures__',
+    'test-project'
+  )
+
+  const execInProject = createExecWithEnvInCwd(TEST_PROJECT_PATH)
 
   console.log(`Cache miss; creating project at ${TEST_PROJECT_PATH}`)
   await fs.copy(TEST_PROJECT_FIXTURE_PATH, TEST_PROJECT_PATH)
