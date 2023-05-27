aa214e610c0ce8b2347353034fd391c95e3f08d1
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-27 13:03:11 -0700

    try splitting up cache

--- .github/actions/set-up-test-project/setUpTestProject.mjs
+++ .github/actions/set-up-test-project/setUpTestProject.mjs
@@ -1,68 +1,78 @@
 /* eslint-env node */
 // @ts-check
 
+import path from 'node:path'
+
 import cache from '@actions/cache'
 import core from '@actions/core'
 import { hashFiles } from '@actions/glob'
 
+import fs from 'fs-extra'
+
+import {
+  createExecWithEnvInCwd,
+  projectCopy,
+  projectDeps,
+  REDWOOD_FRAMEWORK_PATH,
+} from '../actionsLib.mjs'
+
+const TEST_PROJECT_FIXTURE_PATH = path.join(
+  REDWOOD_FRAMEWORK_PATH,
+  '__fixtures__',
+  'test-project'
+)
+
 const TEST_PROJECT_PATH = core.getInput('test-project-path')
 
-const key = [
+const execInProject = createExecWithEnvInCwd(TEST_PROJECT_PATH)
+
+const baseKey = [
   'test-project',
   process.env.RUNNER_OS,
-  await hashFiles('yarn.lock', '.yarnrc.yml'),
-  await hashFiles('packages')
 ].join('-')
 
-console.log({
-  yarnHash: await hashFiles('yarn.lock', '.yarnrc.yml'),
-  packagesHash: await hashFiles('packages')
-})
+const dependenciesKey = [
+  baseKey,
+  await hashFiles('yarn.lock', '.yarnrc.yml'),
+].join('-')
+
+const packagesKey = [
+  dependenciesKey,
+  await hashFiles('packages')
+].join('-')
 
 /**
  * @returns {Promise<void>}
  */
 async function main() {
-  const cacheKey = await cache.restoreCache([TEST_PROJECT_PATH], key)
+  const packagesCacheKey = await cache.restoreCache([TEST_PROJECT_PATH], packagesKey)
 
-  if (cacheKey) {
-    console.log(`Cache restored from key: ${key}`)
+  if (packagesCacheKey) {
+    console.log(`Cache restored from key: ${packagesKey}`)
     return
   }
 
-  console.log(`Cache not found for input keys: ${key}`)
-  console.log(`Creating project at ${TEST_PROJECT_PATH}`)
-  console.log()
+  const dependenciesCacheKey = await cache.restoreCache([TEST_PROJECT_PATH], dependenciesKey)
 
-  await setUpTestProject(TEST_PROJECT_PATH)
+  if (dependenciesCacheKey) {
+    console.log(`Cache restored from key: ${dependenciesKey}`)
+    await sharedTasks()
+  } else {
+    console.log(`Cache not found for input keys: ${packagesKey}, ${dependenciesKey}`)
+    console.log(`Creating project at ${TEST_PROJECT_PATH}`)
+    console.log()
 
-  await cache.saveCache([TEST_PROJECT_PATH], key)
-  console.log(`Cache saved with key: ${key}`)
+    await setUpTestProject(TEST_PROJECT_PATH)
+  }
+
+  await cache.saveCache([TEST_PROJECT_PATH], packagesKey)
+  console.log(`Cache saved with key: ${packagesKey}`)
 }
 
 /**
  * @returns {Promise<void>}
  */
 async function setUpTestProject(testProjectPath) {
-  const path = await import('node:path')
-
-  const { default: fs } = await import('fs-extra')
-
-  const {
-    createExecWithEnvInCwd,
-    projectCopy,
-    projectDeps,
-    REDWOOD_FRAMEWORK_PATH,
-  } = await import('../actionsLib.mjs')
-
-  const TEST_PROJECT_FIXTURE_PATH = path.join(
-    REDWOOD_FRAMEWORK_PATH,
-    '__fixtures__',
-    'test-project'
-  )
-
-  const execInProject = createExecWithEnvInCwd(testProjectPath)
-
   await fs.copy(TEST_PROJECT_FIXTURE_PATH, testProjectPath)
 
   console.log(`Adding framework dependencies to ${testProjectPath}`)
@@ -73,8 +83,15 @@ async function setUpTestProject(testProjectPath) {
   await execInProject('yarn install')
   console.log()
 
+  await cache.saveCache([TEST_PROJECT_PATH], dependenciesKey)
+  console.log(`Cache saved with key: ${dependenciesKey}`)
+
+  await sharedTasks()
+}
+
+async function sharedTasks() {
   console.log('Copying framework packages to project')
-  await projectCopy(testProjectPath)
+  await projectCopy(TEST_PROJECT_PATH)
   console.log()
 
   console.log('Generating dbAuth secret')
@@ -83,7 +100,7 @@ async function setUpTestProject(testProjectPath) {
     { silent: true }
   )
   fs.appendFileSync(
-    path.join(testProjectPath, '.env'),
+    path.join(TEST_PROJECT_PATH, '.env'),
     `SESSION_SECRET='${stdout}'`
   )
   console.log()
@@ -93,5 +110,6 @@ async function setUpTestProject(testProjectPath) {
     'yarn rw prisma migrate reset --force',
   )
 }
+
 
 main()
